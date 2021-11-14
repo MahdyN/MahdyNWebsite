0571b586ee89b316d292788cbeb0a191238f7943
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-11-14 15:19:16 -0500

    Latest Website Update 1

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-document.get
+
+const heat = document.getElementsByTagName("img")[0];
+heat.addEventListener('click', function(){alert('Even though I was born in Brooklyn, I am a fan of the Miami Heat basketball team. Before I moved to Lebanon, I lived in Miami for 4 years and have been a Heat fan since ');});
+
